dacf118007aad5315ea211768b604f9d6a4477d8
devourer <alexblooded@gmail.com>
2020-08-15 20:01:07 +0300

    refactor: move background to separate component

---
+++ components/MainBlock/MainBlockBackground/MainBlockBackground.tsx
@@ -0,0 +1,33 @@
+import styles from "./main-block-background.module.sass"
+import useWindowSize from "@/hooks/useWindowSize"
+import { useMemo } from "react"
+
+const MainBlockBackground = () => {
+    const windowSize = useWindowSize()
+    const backgroundSideLength = useMemo(
+        () =>
+            Math.ceil(
+                Math.sqrt(
+                    2 *
+                        Math.pow(
+                            windowSize.width / 2 + windowSize.height / 2,
+                            2
+                        )
+                )
+            ),
+        [windowSize]
+    )
+
+    return (
+        <div
+            style={{
+                "--sideLength": `${backgroundSideLength}px`,
+            }}
+            className={styles["mainBlockContainer__mainBlockBackground"]}
+        >
+            <div className={styles["mainBlockContainer__backgroundClip"]} />
+        </div>
+    )
+}
+
+export default MainBlockBackground
