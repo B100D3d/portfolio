b01f1b5780a2e5baf5b406dc936a13209cef13dc
devourer <alexblooded@gmail.com>
2020-08-25 17:27:24 +0300

    add: active className implementation to Anchor

--- components/Anchor/Anchor.tsx
+++ components/Anchor/Anchor.tsx
@@ -1,4 +1,7 @@
 import useScrollByBlock from "@hooks/useScrollByBlock"
+import { useCallback, useRef } from "react"
+import classNames from "classnames"
+import { timeout } from "@utils"
 
 interface AnchorProps
     extends React.DetailedHTMLProps<
@@ -7,19 +10,36 @@ interface AnchorProps
     > {
     children: React.ReactNode
     className?: string
+    active?: boolean
+    activeClassName?: string
+    timeout?: number
     to: string
 }
 
 const Anchor: React.FunctionComponent<AnchorProps> = ({
     children,
     className,
+    active = false,
+    activeClassName = "",
+    timeout: ms = 0,
     to,
     ...props
 }) => {
     const { toBlock } = useScrollByBlock()
+    const anchorRef = useRef() as React.RefObject<HTMLAnchorElement>
+
+    const handleClick = useCallback(async () => {
+        await timeout(ms)
+        toBlock(to)
+    }, [toBlock, to, ms])
 
     return (
-        <a onClick={() => toBlock(to)} className={className} {...props}>
+        <a
+            ref={anchorRef}
+            onClick={handleClick}
+            className={classNames(className, { [activeClassName]: active })}
+            {...props}
+        >
             {children}
         </a>
     )
