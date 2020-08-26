cdc9c68fb17e000433ff6be2898e7adbcf4bcc07
devourer <alexblooded@gmail.com>
2020-08-26 18:23:40 +0300

    add: delay prop to ColorFilledText

--- components/ColorFilledText/ColorFilledText.tsx
+++ components/ColorFilledText/ColorFilledText.tsx
@@ -5,17 +5,22 @@ interface ColorFilledProps {
     className?: string
     children: React.ReactNode
     animate?: boolean
+    delay?: string
 }
 
 const ColorFilledText: React.FunctionComponent<ColorFilledProps> = ({
     className = "",
     children,
     animate,
+    delay = "0s",
 }) => {
     return (
         <div className={styles.colorFilled}>
             <span className={styles.colorFilled__back}>{children}</span>
             <span
+                style={{
+                    "--delay": delay,
+                }}
                 data-text={children}
                 className={classNames(styles.colorFilled__front, className, {
                     [styles.animate]: animate,
