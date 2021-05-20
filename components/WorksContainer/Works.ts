c49cf1a5e2d9be520ce38c39a21fa0cfbc914675
B100D3d <alexblooded@gmail.com>
2021-05-20 19:53:28 +0300

    add: cybervoice to works list change: work avatar styles

--- components/WorksContainer/Works.ts
+++ components/WorksContainer/Works.ts
@@ -32,6 +32,10 @@ const MONGODB: Technology = {
     name: "MongoDB",
     icon: "assets/technologies/mongodb.svg",
 }
+const POSTGRES: Technology = {
+    name: "PostgreSQL",
+    icon: "assets/technologies/postgresql.svg",
+}
 const TYPESCRIPT: Technology = {
     name: "Typescript",
     icon: "assets/technologies/typescript.svg",
@@ -40,9 +44,14 @@ const SASS: Technology = {
     name: "Sass",
     icon: "assets/technologies/sass.svg",
 }
+const SCSS: Technology = { ...SASS, name: "SCSS" }
 const GRAPHQL: Technology = {
     name: "GraphQL",
     icon: "assets/technologies/graphql.svg",
+}
+const UIKIT: Technology = {
+    name: "UIkit",
+    icon: "assets/technologies/uikit.svg",
 }
 const MIND: Technology = {
     name: "Mind",
@@ -50,6 +59,22 @@ const MIND: Technology = {
 }
 
 const WORKS: Array<Work> = [
+    {
+        name: "CYBERVOICE",
+        description: "Vocal cords of Artificial Intelligence for everyone.",
+        url: "cybervoice.io",
+        img: "assets/works/cybervoice.webp",
+        technologies: [
+            VUE,
+            VUEX,
+            EXPRESS,
+            POSTGRES,
+            TYPESCRIPT,
+            SCSS,
+            UIKIT,
+            MIND,
+        ],
+    },
     {
         name: "TGMT",
         description: "Personal account for teachers and students",
